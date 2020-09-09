fcfac55e4adef91ddeb3b4d4c0e985a5805d34ef
unknown <ashaduzzaman.nstu1994@gmail.com>
2020-09-09 16:00:04 +0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <template>
-  <!-- <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app> -->
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/add">About</router-link>
+      <router-link to="/add">Add</router-link>
     </div>
     <router-view/>
   </div>
